2c20d4da1fd81fc01101fe98d7646101fbac0bef
Sean Blair <seandibango@gmail.com>
2017-08-12 14:46:14 -0700

    Implement statusTextView for messages at bottom of screen, add methods triggered by start and stop buttons

--- app/src/main/java/com/sean/mymetronome/MainActivity.java
+++ app/src/main/java/com/sean/mymetronome/MainActivity.java
@@ -2,6 +2,8 @@ package com.sean.mymetronome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when user taps start **/
+    public void start(View view) {
+        setStatusText("Start button pressed.");
+    }
+
+    /** Called when user taps stop **/
+    public void stop(View view) {
+        setStatusText("Stop button pressed.");
+    }
+
+    private void setStatusText(String str) {
+        TextView statusTextView = (TextView) findViewById(R.id.statusText);
+        statusTextView.setText(str);
     }
 }
